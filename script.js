0867a1fe630d770f57cd46ffd36870d7bfbde977
vidhyasagar <vs1522250@gmail.com>
2026-05-11 17:10:33 +0530

    Two Good Co.

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
-gsap.registerPlugin(ScrollTrigger);
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("main"),
-  smooth: true,
-});
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-locoScroll.on("scroll", ScrollTrigger.update);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-ScrollTrigger.scrollerProxy("main", {
-  scrollTop(value) {
-    return arguments.length
-      ? locoScroll.scrollTo(value, 0, 0)
-      : locoScroll.scroll.instance.scroll.y;
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+locomotiveAnimation();
+
+gsap.to("nav .nav-part1 svg", {
+  transform: "translate(-100%)",
+  scrollTrigger: {
+    scroller: "main",
+    trigger: "page1",
+    markers: true,
+    start: "top 0",
+    end: "bottom -5%",
+    scrub: true,
   },
-
-  getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  },
-
-  pinType: document.querySelector("main").style.transform
-    ? "transform"
-    : "fixed",
 });
-
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-let mouseX = 0;
-let mouseY = 0;
 
 function imageAnimate() {
   let image = document.querySelector(".image");
@@ -91,40 +109,46 @@ function loadingAnimation() {
 
 loadingAnimation();
 
-gsap.to(".image img", {
-  y: 200,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".image",
-    scroller: "main",
-    start: "top bottom",
-    end: "bottom top",
-    scrub: 2,
-  },
-});
-
-ScrollTrigger.refresh();
-
-document.addEventListener("mousemove", function (dets) {
-  gsap.to(".cursor", {
-    left: dets.x,
-    top: dets.y,
+function imageAnimation() {
+  gsap.to(".image img", {
+    y: 200,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".image",
+      scroller: "main",
+      start: "top bottom",
+      end: "bottom top",
+      scrub: 2,
+    },
   });
-});
+}
 
-document.querySelectorAll(".child").forEach(function (elem) {
-  elem.addEventListener("mouseenter", function () {
+imageAnimation();
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
     gsap.to(".cursor", {
-      transform: "translate(-50%,-50%)",
-      scale: 1,
+      left: dets.x,
+      top: dets.y,
     });
   });
 
-  elem.addEventListener("mouseleave", function () {
-    gsap.to(".cursor", {
-      duration: 0.3,
-      transform: "translate(-50%,-50%)",
-      scale: 0,
+  document.querySelectorAll(".child").forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to(".cursor", {
+        transform: "translate(-50%,-50%)",
+        scale: 1,
+      });
+    });
+
+    elem.addEventListener("mouseleave", function () {
+      gsap.to(".cursor", {
+        duration: 0.3,
+        transform: "translate(-50%,-50%)",
+        scale: 0,
+      });
     });
   });
-});
+}
+
+cursorAnimation();
